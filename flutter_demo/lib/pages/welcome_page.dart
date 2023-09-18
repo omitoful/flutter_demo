70b04512597f6b57a1318247df722fa5cdf4967c
Kevin <kevin@twjoin.com>
2023-09-18 18:27:56 +0800

    basic setup & welcome page

---
+++ flutter_demo/lib/pages/welcome_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_demo/misc/colors.dart';
+import 'package:flutter_demo/widgets/app_large_text.dart';
+import 'package:flutter_demo/widgets/app_text.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage("img/" + images[index]),
+                fit: BoxFit.cover,
+              )),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppLargeText(text: "Trips"),
+                        AppText(
+                          text: "Mountain",
+                          size: 30,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: 250,
+                          child: AppText(
+                            text:
+                                "Mountain hike gives you an incredible sense of freedom along with endurance tests.",
+                            color: AppColors.textColor2,
+                            size: 14,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
