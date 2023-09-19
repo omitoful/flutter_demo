61f96f149b42fab1ea5c4372d5fd10c39d732802
Kevin <kevin@twjoin.com>
2023-09-19 14:55:05 +0800

    homepage tabbar & indicator setting by using painter

--- flutter_demo/lib/pages/nav_pages/home_page.dart
+++ flutter_demo/lib/pages/nav_pages/home_page.dart
@@ -1,12 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/misc/colors.dart';
+import 'package:flutter_demo/widgets/app_large_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
   @override State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override Widget build(BuildContext context) {
-    return Container();
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Menu Icon
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              children: [
+                Icon(Icons.menu, size: 30, color: Colors.black54),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 40),
+          // Discover Text
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(text: "Discover"),
+          ),
+          SizedBox(height: 40),
+          // Tabbar
+          Container(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                labelPadding: const EdgeInsets.only(left: 20, right: 20),
+                controller: _tabController,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                isScrollable: true,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicator: CircleTabIndicator(color: AppColors.mainColor, radius: 4),
+                tabs: [
+                  Tab(text: "Places"),
+                  Tab(text: "Inspiration"),
+                  Tab(text: "Emotions"),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: double.maxFinite,
+            height: 300,
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                Text("Hi"),
+                Text("There"),
+                Text("Bye"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+  CircleTabIndicator({required this.color, required this.radius});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    // TODO: implement createBoxPainter
+    return _CirclePainter(color: color, radius: radius);
+  }
+}
+
+class _CirclePainter extends BoxPainter {
+  final Color color;
+  double radius;
+  _CirclePainter({required this.color, required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+    final Offset circleOffset = Offset(configuration.size!.width/2 - radius/2, configuration.size!.height - radius);
+
+    canvas.drawCircle(offset + circleOffset, radius, _paint);
   }
 }
